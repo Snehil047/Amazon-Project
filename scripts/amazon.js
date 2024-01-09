5a2d7ab9fc54a4c1b895897962648ed90fbc8e79
Snehil Maurya <snehilwilldi@gmail.com>
2024-01-09 15:15:44 +0530

    all products generated

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -6,35 +6,47 @@
 
 // STEP 01 -> SAVE THE DATA
 
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: " Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 1090, //javascript have trouble doing calculations in decimal hence we use cents insted of dollars.
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: " Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-];
+// const products = [
+//   {
+//     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+//     name: " Black and Gray Athletic Cotton Socks - 6 Pairs",
+//     rating: {
+//       stars: 4.5,
+//       count: 87,
+//     },
+//     priceCents: 1090, //javascript have trouble doing calculations in decimal hence we use cents insted of dollars.
+//   },
+//   {
+//     image: "images/products/intermediate-composite-basketball.jpg",
+//     name: " Intermediate Size Basketball",
+//     rating: {
+//       stars: 4,
+//       count: 127,
+//     },
+//     priceCents: 2095,
+//   },
+//   {
+//     image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+//     name: "Adults Plain Cotton T-Shirt - 2 Pack",
+//     rating: {
+//       stars: 4.5,
+//       count: 56,
+//     },
+//     priceCents: 799,
+//   },
+//   {
+//     image: "images/products/black-2-slot-toaster.jpg",
+//     name: "2 Slot Toaster Black",
+//     rating: {
+//       stars: 5,
+//       count: 2197,
+//     },
+//     priceCents: 1899,
+//   },
+//   {
+//     image: "",
+//   },
+// ];
 
 //STEP 02 -> GENERATING THE HTML
 // we're going to use the above data to generate HTML, instead of doing that manually.
