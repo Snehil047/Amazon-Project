748d52380eb24f17a38e1e65d6f234784b2d75b2
Snehil Maurya <snehilwilldi@gmail.com>
2024-01-09 11:04:13 +0530

    Comments

---
+++ scripts/amazon.js
@@ -0,0 +1,5 @@
+// Main Idea of Javascript,
+
+//     1. Save The Data -> Create something that closely matches the data, like if there's a list make it save in an array, if the list has multiple attributes, save it in an Object.
+//     2. Generate The HTML
+//     3. Make It Interactive
